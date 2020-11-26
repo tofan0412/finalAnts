fdf51e40a5698628cde46e5f785feeb66fabfc25
LAPTOP-OEHRGEL3\noyli <noylit11@gmail.com>
2020-11-26 17:18:48 +0900

    전체 VO 추가

--- src/main/java/ants/com/admin/model/IpVo.java
+++ src/main/java/ants/com/admin/model/IpVo.java
@@ -1,5 +1,45 @@
 package ants.com.admin.model;
 
 public class IpVo {
-
+	
+	
+	private String ip_id;
+	private String ip_addr;
+	private String ip_status;
+	private String admin_id;
+	
+	
+	public String getIp_id() {
+		return ip_id;
+	}
+	public void setIp_id(String ip_id) {
+		this.ip_id = ip_id;
+	}
+	public String getIp_addr() {
+		return ip_addr;
+	}
+	public void setIp_addr(String ip_addr) {
+		this.ip_addr = ip_addr;
+	}
+	public String getIp_status() {
+		return ip_status;
+	}
+	public void setIp_status(String ip_status) {
+		this.ip_status = ip_status;
+	}
+	public String getAdmin_id() {
+		return admin_id;
+	}
+	public void setAdmin_id(String admin_id) {
+		this.admin_id = admin_id;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "IpVo [ip_id=" + ip_id + ", ip_addr=" + ip_addr + ", ip_status=" + ip_status + ", admin_id=" + admin_id
+				+ "]";
+	}
+	
+	
 }
--- src/main/java/ants/com/board/memBoard/model/ReplyVo.java
+++ src/main/java/ants/com/board/memBoard/model/ReplyVo.java
@@ -1,5 +1,77 @@
 package ants.com.board.memBoard.model;
 
-public class ReplyVo {
+import java.util.Date;
 
+public class ReplyVo {
+	
+	
+	private String reply_id;
+	private String some_id;
+	private String reply_cont;
+	private Date reg_dt;
+	private String del;
+	private String category_id;
+	private String req_id;
+	private String mem_id;
+	
+	
+	public String getReply_id() {
+		return reply_id;
+	}
+	public void setReply_id(String reply_id) {
+		this.reply_id = reply_id;
+	}
+	public String getSome_id() {
+		return some_id;
+	}
+	public void setSome_id(String some_id) {
+		this.some_id = some_id;
+	}
+	public String getReply_cont() {
+		return reply_cont;
+	}
+	public void setReply_cont(String reply_cont) {
+		this.reply_cont = reply_cont;
+	}
+	public Date getReg_dt() {
+		return reg_dt;
+	}
+	public void setReg_dt(Date reg_dt) {
+		this.reg_dt = reg_dt;
+	}
+	public String getDel() {
+		return del;
+	}
+	public void setDel(String del) {
+		this.del = del;
+	}
+	public String getCategory_id() {
+		return category_id;
+	}
+	public void setCategory_id(String category_id) {
+		this.category_id = category_id;
+	}
+	public String getReq_id() {
+		return req_id;
+	}
+	public void setReq_id(String req_id) {
+		this.req_id = req_id;
+	}
+	public String getMem_id() {
+		return mem_id;
+	}
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "ReplyVo [reply_id=" + reply_id + ", some_id=" + some_id + ", reply_cont=" + reply_cont + ", reg_dt="
+				+ reg_dt + ", del=" + del + ", category_id=" + category_id + ", req_id=" + req_id + ", mem_id=" + mem_id
+				+ "]";
+	}
+	
+	
+	
 }
