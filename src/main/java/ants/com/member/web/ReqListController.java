7fdfb80df6cf60a5227efb39dc51512c93706d42
LMH <2mh4000@gmail.com>
2020-11-28 13:32:56 +0900

    reqController변경

--- src/main/java/ants/com/member/web/ReqListController.java
+++ src/main/java/ants/com/member/web/ReqListController.java
@@ -2,17 +2,28 @@ package ants.com.member.web;
 
 
 
+import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ants.com.member.service.PmServiceI;
 
-@RequestMapping("/reqList")
+
+@RequestMapping("/req")
 @Controller
 public class ReqListController {
 	private static final Logger logger = LoggerFactory.getLogger(ReqListController.class);
 	
+	@Resource(name="pmService")
+	private PmServiceI pmService;
+	
+	@RequestMapping("/reqList")
+	public String selectReqList() {
+		return "member/pmReqList";
+	}
 
 	
 }
