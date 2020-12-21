56b4b6ee3fd991c9d24b85ebaf3f8fe6eadc1dbf
bani <bban7637@gmail.com>
2020-12-21 12:40:49 +0900

    주석추가

--- src/main/java/ants/com/member/web/ProjectController.java
+++ src/main/java/ants/com/member/web/ProjectController.java
@@ -180,6 +180,7 @@ public class ProjectController {
 		return "redirect:/project/outlineView";
 	}
 
+	//개요페이지
 	@RequestMapping("/outlineView")
 	public String outlineView(HttpSession session, Model model) {
 		String reqId = (String) session.getAttribute("projectId");
@@ -208,7 +209,7 @@ public class ProjectController {
 		return "tiles/chart/chartmain";
 	}
 
-	// 차트 뷰
+	// 팀원별 할일 진행도
 	@RequestMapping("/stackedbarchart")
 	public String stackedbarchart(Model model, TodoVo todoVo, HttpSession session) {
 		String reqId = (String) session.getAttribute("projectId");
@@ -220,7 +221,7 @@ public class ProjectController {
 		return "jsonView";
 	}
 
-	// 차트 뷰
+	// 팀원별 할일 비중도
 	@RequestMapping("/donutChart")
 	public String donutChart(Model model, TodoVo todoVo, HttpSession session) {
 		String reqId = (String) session.getAttribute("projectId");
@@ -232,7 +233,7 @@ public class ProjectController {
 		return "jsonView";
 	}
 
-	// 차트 뷰
+	// 건의사항 수용, 거절, 대기 율
 	@RequestMapping("/donutSuggestAccept")
 	public String donutSuggestAccept(Model model, SuggestVo suggestVo, HttpSession session) {
 		String reqId = (String) session.getAttribute("projectId");
@@ -245,6 +246,7 @@ public class ProjectController {
 		return "jsonView";
 	}
 
+	// 팀원별 건의사항 작성 수
 	@RequestMapping("/barchartSuggestCnt")
 	public String chartsuggestcnt(Model model, SuggestVo suggestVo, HttpSession session) {
 		String reqId = (String) session.getAttribute("projectId");
@@ -255,7 +257,7 @@ public class ProjectController {
 		return "jsonView";
 	}
 
-	// 차트 뷰
+	// 전체 투표 참여율
 	@RequestMapping("/donutchartvotetotal")
 	public String donutchartvotetotal(Model model, VoteVo voteVo, HttpSession session) {
 		String reqId = (String) session.getAttribute("projectId");
@@ -264,6 +266,7 @@ public class ProjectController {
 		return "jsonView";
 	}
 
+	// 개인별 투표참여율
 	@RequestMapping("/barchartvoteindi")
 	public String barchartvoteindi(Model model, VoteVo voteVo, HttpSession session) {
 		String reqId = (String) session.getAttribute("projectId");
@@ -274,6 +277,7 @@ public class ProjectController {
 		return "jsonView";
 	}
 	
+	// 일별 파일 업로드용량
 	@RequestMapping("/chartfilesday")
 	public String chartfilesday_mb(Model model, PublicFileVo publicFileVo, HttpSession session) {
 		String reqId = (String) session.getAttribute("projectId");
@@ -284,6 +288,7 @@ public class ProjectController {
 		return "jsonView";
 	}
 	
+	// 월별 파일 업로드 용량
 	@RequestMapping("/chartfilesmonth")
 	public String chartfilesmonth(Model model, PublicFileVo publicFileVo, HttpSession session) {
 		String reqId = (String) session.getAttribute("projectId");
@@ -294,6 +299,7 @@ public class ProjectController {
 		return "jsonView";
 	}
 	
+	// 확장자별 업로드 수
 	@RequestMapping("/chartextension")
 	public String chartfilesextension(Model model, PublicFileVo publicFileVo, HttpSession session) {
 		String reqId = (String) session.getAttribute("projectId");
@@ -304,6 +310,7 @@ public class ProjectController {
 		return "jsonView";
 	}
 	
+	// 일별 이슈 작성수
 	@RequestMapping("/chartIssuesday")
 	public String chartIssuesday(Model model,IssueVo issueVo, HttpSession session) {
 		String reqId = (String) session.getAttribute("projectId");
@@ -314,6 +321,7 @@ public class ProjectController {
 		return "jsonView";
 	}
 	
+	// 월별 이슈 작성수
 	@RequestMapping("/chartIssuesmonth")
 	public String chartIssuesmonth(Model model, IssueVo issueVo, HttpSession session) {
 		String reqId = (String) session.getAttribute("projectId");
@@ -324,6 +332,7 @@ public class ProjectController {
 		return "jsonView";
 	}
 	
+	// 팀원별 이슈 작성수
 	@RequestMapping("/chartIssuesCnt")
 	public String chartIssuesCnt(Model model, IssueVo issueVo, HttpSession session) {
 		String reqId = (String) session.getAttribute("projectId");
