90c236ed9af1c6d9d81b830113661e216469b265
tofan0412 <tofan12312@gmail.com>
2020-12-29 10:51:06 +0900

    20.12.29 - 필요없는 파일 삭제 - EchoHandler logger 삭제

--- src/main/java/ants/com/chatting/ws/EchoHandler.java
+++ src/main/java/ants/com/chatting/ws/EchoHandler.java
@@ -5,8 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -14,22 +12,19 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import ants.com.member.model.MemberVo;
-import edu.emory.mathcs.backport.java.util.concurrent.ConcurrentHashMap;
 
 @RequestMapping("/echo")
 public class EchoHandler extends TextWebSocketHandler {
 	
-	private static final Logger LOGGER = LoggerFactory.getLogger(EchoHandler.class);
 	private List<WebSocketSession> sessionList = new ArrayList<WebSocketSession>();
 	
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) 
 			throws Exception {
 		// 로그인한 정보 넣기
-		Map<String, Object> httpSession = session.getAttributes();
-		MemberVo SMEMBER = (MemberVo) httpSession.get("SMEMBER");
+//		Map<String, Object> httpSession = session.getAttributes();
+//		MemberVo SMEMBER = (MemberVo) httpSession.get("SMEMBER");
 		
-//		LOGGER.info("{} 연결 됨\n. 현재 채팅방 번호 : {}", SMEMBER.getMemId(), httpSession.get("cgroupId"));
 		sessionList.add(session);
 		
 	}
@@ -40,9 +35,6 @@ public class EchoHandler extends TextWebSocketHandler {
 		Map<String, Object> httpSession = session.getAttributes();
 		MemberVo SMEMBER = (MemberVo) httpSession.get("SMEMBER");
 		String cgroupId = (String) httpSession.get("cgroupId");
-		
-//		LOGGER.info("{}로부터 \'{}\' 받음\n", SMEMBER.getMemId(), message.getPayload());
-//		LOGGER.info("채팅방 번호 : {}", cgroupId);
 		
 		// sessionList에 존재하는 모든 사용자에게 메시지 뿌리기 ..
 		for(WebSocketSession sess : sessionList) {
@@ -58,11 +50,9 @@ public class EchoHandler extends TextWebSocketHandler {
 	@Override
 	public void afterConnectionClosed(
 			WebSocketSession session, CloseStatus status) throws Exception {
-		Map<String, Object> httpSession = session.getAttributes();
-		MemberVo SMEMBER = (MemberVo) httpSession.get("SMEMBER");
+//		Map<String, Object> httpSession = session.getAttributes();
+//		MemberVo SMEMBER = (MemberVo) httpSession.get("SMEMBER");
 		
 		sessionList.remove(session);
-//		LOGGER.info("{} 연결 끊김\n", SMEMBER.getMemId());
 	}
-	
 }
